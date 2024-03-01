c29baf6fd151c1f890759e2de1c39b9f9d19d545
David120000 <david.remetei@gmail.com>
2024-03-01 22:27:17 +0100

    gateway service authentication-client

---
+++ gateway/src/main/java/rd/gateway/AuthenticationClient.java
@@ -0,0 +1,31 @@
+package rd.gateway;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+@Component
+public class AuthenticationClient {
+    
+    @Value("${application.config.authenticationservice-url}")
+    private String authenticationServiceURL;
+    private final RestClient restClient = RestClient.create();
+
+
+    public Authorization authorize(AuthToken token) throws RuntimeException {
+
+        var authorization = restClient.post()
+            .uri(authenticationServiceURL)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(token)
+            .retrieve()
+            .onStatus(HttpStatusCode::is4xxClientError, (request, response) -> {
+                throw new RuntimeException("Error: " + response.getStatusCode() + " :: " + response.getStatusText());
+            })
+            .body(Authorization.class);
+
+        return authorization;
+    }
+}
