732c5ac88326e44ce2980df28a2ac1962ab9779e
David120000 <david.remetei@gmail.com>
2024-03-01 21:10:30 +0100

    postservice model and repository layer

---
+++ post/src/main/java/rd/post/Post.java
@@ -0,0 +1,29 @@
+package rd.post;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Document(collection = "posts")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Post {
+    
+    @Id
+    private String id;
+    private String authorEmail;
+    private LocalDateTime createdAt;
+    private String content;
+    private boolean published;
+
+}
---
+++ post/src/main/java/rd/post/PostRepository.java
@@ -0,0 +1,15 @@
+package rd.post;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+    Page<Post> findByPublishedOrderByCreatedAtDesc(boolean published, Pageable pageable);
+    Page<Post> findByAuthorEmailOrderByCreatedAtDesc(String authorEmail, Pageable pageable);
+    Page<Post> findByAuthorEmailAndPublishedOrderByCreatedAtDesc(String authorEmail, boolean published, Pageable pageable);
+
+ }
