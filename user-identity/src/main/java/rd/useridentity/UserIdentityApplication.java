ffdb499a9e5230fd9d63b21e2e40c22136606be2
David120000 <david.remetei@gmail.com>
2024-03-01 20:12:32 +0100

    userservice project init

---
+++ user-identity/src/main/java/rd/useridentity/UserIdentityApplication.java
@@ -0,0 +1,13 @@
+package rd.useridentity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserIdentityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserIdentityApplication.class, args);
+	}
+
+}
---
+++ user-identity/src/test/java/rd/useridentity/UserIdentityApplicationTests.java
@@ -0,0 +1,13 @@
+package rd.useridentity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserIdentityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
