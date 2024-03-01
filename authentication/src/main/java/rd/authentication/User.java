0f6e4434588c0dff7523ac2e8940d22ed8d58ea8
David120000 <david.remetei@gmail.com>
2024-03-01 21:46:59 +0100

    authenticationservice model layer

---
+++ authentication/src/main/java/rd/authentication/User.java
@@ -0,0 +1,17 @@
+package rd.authentication;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class User {
+    
+    private String email;
+    private String password;
+    private String role;
+    private boolean enabled;
+    
+}
---
+++ authentication/src/main/java/rd/authentication/UserAuthorization.java
@@ -0,0 +1,16 @@
+package rd.authentication;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class UserAuthorization {
+    
+    private String subjectId;
+    private Boolean authenticated;
+    private String role;
+
+}
