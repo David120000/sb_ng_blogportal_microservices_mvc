e13eb22b97ba31a2b6129a26393b98eae1dfaac4
David120000 <david.remetei@gmail.com>
2024-03-04 17:08:09 +0100

    ng service component-data-communication authobject

--- frontend-blog-client/src/app/services/component-communication.service.ts
+++ frontend-blog-client/src/app/services/component-communication.service.ts
@@ -1,9 +1,48 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
+import { AuthenticatedUser } from '../models/authenticated-user';
+import { Observable, Subject, Subscription } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ComponentCommunicationService {
+export class ComponentCommunicationService implements OnDestroy {
 
-  constructor() { }
+  private authenticatedUser: AuthenticatedUser;
+  private readonly authObjectSource: Subject<AuthenticatedUser>;
+  private readonly authObjectObservable$: Observable<AuthenticatedUser>;
+
+  private authObjectSubscription: Subscription;
+
+  constructor() { 
+    
+    this.authenticatedUser = new AuthenticatedUser(undefined, undefined);
+    this.authObjectSource = new Subject<AuthenticatedUser>;
+    this.authObjectObservable$ = this.authObjectSource.asObservable();
+
+    this.authObjectSubscription = this.authObjectObservable$.subscribe(
+      newAuthenticatedUser => this.authenticatedUser = newAuthenticatedUser
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.authObjectSubscription.unsubscribe();
+  }
+
+
+  public setAuthentication(authentication: AuthenticatedUser) {
+    this.authObjectSource.next(authentication);
+  }
+
+  public clearAuthentication() {
+    this.authObjectSource.next(new AuthenticatedUser(undefined, undefined));
+  }
+
+  public getAuthentication(): AuthenticatedUser {
+    return this.authenticatedUser;
+  }
+
+  public getAuthenticationObservable(): Observable<AuthenticatedUser> {
+    return this.authObjectObservable$;
+  }
+
 }
