24dee312529d21c91b11748a172e24866ec2b2da
David120000 <david.remetei@gmail.com>
2024-03-04 15:49:11 +0100

    ng service jwt-parser

--- frontend-blog-client/src/app/services/jwt-parser.service.ts
+++ frontend-blog-client/src/app/services/jwt-parser.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { AuthToken } from '../models/auth-token';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JwtParserService {
 
-  constructor() { }
+  constructor(private jwtHelperService: JwtHelperService) { }
+
+
+  public getSubjectId(securityToken: AuthToken): string | undefined {
+
+    let subject = undefined;
+    if(securityToken.jwt != undefined) {
+      subject = this.jwtHelperService.decodeToken(securityToken.jwt).sub;
+    }
+
+    return subject;
+  }
 }
