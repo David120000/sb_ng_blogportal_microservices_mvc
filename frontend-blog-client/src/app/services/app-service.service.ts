27d64a7c84ee9382d59889a8d226b49fe7e4aa52
David120000 <david.remetei@gmail.com>
2024-03-05 14:28:14 +0100

    ng service main-service skeleton

--- frontend-blog-client/src/app/services/app-service.service.ts
+++ frontend-blog-client/src/app/services/app-service.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { RestClientService } from './rest-client.service';
+import { JwtParserService } from './jwt-parser.service';
+import { AuthenticationObjectService } from './authentication-object.service';
+import { PostsCacheService } from './posts-cache.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppServiceService {
 
-  constructor() { }
+  constructor(
+    private restClient: RestClientService, 
+    private jwtParser: JwtParserService, 
+    private authObjectService: AuthenticationObjectService, 
+    private postsCache: PostsCacheService
+    ) { }
+
+
+  public registerUser() {
+
+  }
+
+  public authenticateUser() {
+
+  }
+
+  public getAuthentication() {
+
+  }
+
+  public getAuthenticationObservable() {
+
+  }
+
+  public getPosts() {
+
+  }
+
+  public newPost() {
+    
+  }
+
 }
