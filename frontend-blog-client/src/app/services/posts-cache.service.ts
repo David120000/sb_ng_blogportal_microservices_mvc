54ffde87fae7a669ba21380837026197a5ecda0a
David120000 <david.remetei@gmail.com>
2024-03-05 14:25:31 +0100

    ng service posts-cache

--- frontend-blog-client/src/app/services/posts-cache.service.ts
+++ frontend-blog-client/src/app/services/posts-cache.service.ts
@@ -1,9 +1,59 @@
 import { Injectable } from '@angular/core';
+import { UserProfileDTO } from '../models/user-profile-dto';
+import { Post } from '../models/post';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsCacheService {
 
-  constructor() { }
+  private posts: Array<Post>;
+  private authors: Map<string, UserProfileDTO>;
+
+  constructor() {
+    this.posts = new Array<Post>();
+    this.authors = new Map<string, UserProfileDTO>();
+  }
+
+
+  public addToPostsCache(post: Post) {
+    
+    if(!this.posts.includes(post)) {
+      this.posts.push(post);
+    }
+  }
+
+  public addFirstToPostsCache(post: Post) {
+    
+    if(this.posts.length > 0) {
+      // for better performance, addFirst only checks the current first element and not the whole array with includes()
+      if(this.posts.at(0)?.id != post.id) {
+        this.posts.unshift(post);
+      }
+    }
+    else {
+      this.posts.unshift(post);
+    } 
+  }
+
+  public getPosts(): Array<Post> {
+    return this.posts;
+  }
+
+
+  public addToAuthorsCache(profile: UserProfileDTO) {
+    
+    this.authors.set(
+      (profile.email) ? profile.email : "nokey", 
+      profile
+    );
+  }
+
+  public getAuthor(email: string): UserProfileDTO | undefined {
+    return this.authors.get(email);
+  }
+
+  public hasAuthor(email: string): boolean {
+    return this.authors.has(email);
+  }
 }
