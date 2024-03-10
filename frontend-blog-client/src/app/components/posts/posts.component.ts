7ea848d6ad87de1659034f5769f1079cc0e78ef5
David120000 <david.remetei@gmail.com>
2024-03-10 13:27:49 +0100

    ng component Posts functionality extended

--- frontend-blog-client/src/app/components/posts/posts.component.ts
+++ frontend-blog-client/src/app/components/posts/posts.component.ts
@@ -1,6 +1,8 @@
 import { Component, DoCheck, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { AuthenticatedUser } from 'src/app/models/authenticated-user';
+import { CompletionStatusInformation } from 'src/app/models/completion-status-information';
 import { Post } from 'src/app/models/post';
 import { UserProfileDTO } from 'src/app/models/user-profile-dto';
 import { AppServiceService } from 'src/app/services/app-service.service';
@@ -15,19 +17,31 @@ export class PostsComponent implements OnInit, OnDestroy {
   private authenticatedUser: AuthenticatedUser;
   private authenticationSubscription: Subscription | undefined;
 
+  private filterResultsByAuthor: string | null | undefined;
+  private onlyListOwnPosts: boolean;
+
   private postsPage: number;
   private readonly pageSize: number;
-  private posts: Array<Post>;
+  private firstFetchInProgress: boolean;
+  private preventAutomatedFetch: boolean;
 
   private loadButtonDisabled: boolean;
   private message: string | undefined;
 
 
-  constructor(private service: AppServiceService) {
+  constructor(private service: AppServiceService, private route: ActivatedRoute) {
+    
     this.authenticatedUser = this.service.getAuthentication();
-    this.postsPage = Math.floor(this.service.getPosts().length / 6);
-    this.pageSize = 6;
-    this.posts = this.service.getPosts();
+    
+    this.filterResultsByAuthor = undefined;
+    this.onlyListOwnPosts = false;
+    
+    this.pageSize = 4;
+    this.postsPage = Math.floor(this.service.getPosts().length / this.pageSize); 
+    
+    this.firstFetchInProgress = false;
+    this.preventAutomatedFetch = false;
+    
     this.loadButtonDisabled = false;
   }
 
@@ -36,10 +50,6 @@ export class PostsComponent implements OnInit, OnDestroy {
     this.authenticationSubscription = 
       this.service.getAuthenticationObservable()
         .subscribe(newAuthObject => this.authenticatedUser = newAuthObject);
-    
-    if(this.isUserAuthenticated()) {
-      this.service.fetchPosts(this.postsPage, this.pageSize);
-    }
   }
 
   ngOnDestroy(): void {
@@ -53,9 +63,20 @@ export class PostsComponent implements OnInit, OnDestroy {
 
   public getPosts(): Array<Post> {
 
-    if(this.service.getPosts().length === 0) {
-      this.postsPage = 0;
-      this.service.fetchPosts(this.postsPage, this.pageSize);
+    this.handleRouteChanges();
+
+    if(this.service.getPosts().length === 0 && this.firstFetchInProgress === false && !this.preventAutomatedFetch) {
+      
+      this.firstFetchInProgress = true;
+      this.fetchPostsNextPage()
+        .then((response) => {
+
+          if(!response.executedSuccessfully) {     
+            this.message = response.message;
+            this.preventAutomatedFetch = true;
+          }
+        })
+        .finally(() => this.firstFetchInProgress = false);
     }
 
     return this.service.getPosts();
@@ -75,7 +96,7 @@ export class PostsComponent implements OnInit, OnDestroy {
     this.loadButtonDisabled = true;
     this.postsPage++;
 
-    let response = await this.service.fetchPosts(this.postsPage, this.pageSize);
+    let response = await this.fetchPostsNextPage();
 
     if(response.executedSuccessfully) {
       // html element formatting
@@ -89,6 +110,32 @@ export class PostsComponent implements OnInit, OnDestroy {
 
   public getAuthorProfile(email: string): UserProfileDTO {
     return this.service.getProfileByEmail(email);
+  }
+
+  private async fetchPostsNextPage(): Promise<CompletionStatusInformation> {
+    
+    return (this.onlyListOwnPosts) ?
+      this.service.fetchPosts(this.postsPage, this.pageSize, this.authenticatedUser.subjectId, this.onlyListOwnPosts) :
+        (this.filterResultsByAuthor !== null) ? this.service.fetchPosts(this.postsPage, this.pageSize, this.filterResultsByAuthor) : 
+          this.service.fetchPosts(this.postsPage, this.pageSize);
+  }
+
+  private handleRouteChanges() {
+
+    let ownPostsUrlMatch = (this.route.snapshot.url[1] !== undefined && this.route.snapshot.url[1].toString() === "posts-by-me");
+    let routeAuthorParam = this.route.snapshot.paramMap.get('author');
+
+    if(ownPostsUrlMatch && ownPostsUrlMatch !== this.onlyListOwnPosts) {
+      this.service.clearPostsCache();
+      this.postsPage = 0;  
+    }
+    else if(routeAuthorParam !== this.filterResultsByAuthor) {
+      this.service.clearPostsCache();
+      this.postsPage = 0;
+      this.preventAutomatedFetch = false;
+      this.filterResultsByAuthor = routeAuthorParam;
+    }
+    this.onlyListOwnPosts = ownPostsUrlMatch;
   }
 
 }
