fa5684fe63e44697e0cf82a027397d50665370bc
David120000 <david.remetei@gmail.com>
2024-03-13 17:59:56 +0100

    ng component posts view

--- frontend-blog-client/src/app/components/posts/posts.component.ts
+++ frontend-blog-client/src/app/components/posts/posts.component.ts
@@ -1,4 +1,4 @@
-import { Component, DoCheck, OnDestroy, OnInit } from '@angular/core';
+import { Component, DoCheck, ElementRef, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { AuthenticatedUser } from 'src/app/models/authenticated-user';
@@ -27,9 +27,19 @@ export class PostsComponent implements OnInit, OnDestroy {
 
   private loadButtonDisabled: boolean;
   private message: string | undefined;
+  private fetchError: boolean;
+  private fetchMessageVisible: boolean;
+  private timeOutID: ReturnType<typeof setTimeout> | undefined;
+
+  private lineColors: Array<string>;
 
 
-  constructor(private service: AppServiceService, private route: ActivatedRoute) {
+  constructor(
+    private service: AppServiceService, 
+    private route: ActivatedRoute,
+    private elementRef: ElementRef,
+    private renderer: Renderer2
+    ) {
     
     this.authenticatedUser = this.service.getAuthentication();
     
@@ -43,6 +53,13 @@ export class PostsComponent implements OnInit, OnDestroy {
     this.preventAutomatedFetch = false;
     
     this.loadButtonDisabled = false;
+    this.fetchError = false;
+    this.fetchMessageVisible = true;
+
+    this.lineColors = [
+      '#2196F3', '#32c787', '#00BCD4', '#ff5652',
+      '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
+    ];
   }
 
 
@@ -56,23 +73,21 @@ export class PostsComponent implements OnInit, OnDestroy {
     this.authenticationSubscription?.unsubscribe();
   }
 
-
-  public isUserAuthenticated(): boolean {
-    return this.authenticatedUser.hasToken();
-  }
-
   public getPosts(): Array<Post> {
 
     this.handleRouteChanges();
 
     if(this.service.getPosts().length === 0 && this.firstFetchInProgress === false && !this.preventAutomatedFetch) {
       
+      clearTimeout(this.timeOutID);
       this.firstFetchInProgress = true;
+      
       this.fetchPostsNextPage()
         .then((response) => {
 
           if(!response.executedSuccessfully) {     
             this.message = response.message;
+            this.fetchError = true;
             this.preventAutomatedFetch = true;
           }
         })
@@ -82,34 +97,43 @@ export class PostsComponent implements OnInit, OnDestroy {
     return this.service.getPosts();
   }
 
-  public isLoadButtonDisabled(): boolean {
-    return this.loadButtonDisabled;
-  }
-
-  public getMessage(): string | undefined {
-    return this.message;
-  }
-
   public async loadMorePosts() {
     
+    clearTimeout(this.timeOutID);
     this.message = undefined;
+    this.fetchError = false;
     this.loadButtonDisabled = true;
+
     this.postsPage++;
 
     let response = await this.fetchPostsNextPage();
 
-    if(response.executedSuccessfully) {
-      // html element formatting
-    }
-    else {
-      // html element formatting
-    }
+    this.fetchError = response.executedSuccessfully;
     this.message = response.message;
+    this.fetchMessageVisible = true;
     this.loadButtonDisabled = false;
+
+    this.timeOutID = setTimeout(() => {
+        this.fetchMessageVisible = false;
+      }, 
+      3500
+    ); // I will hide the message after the timeout, but I won't change the text back to undefined in an other timeout to save one unnecessary re-render.
   }
 
   public getAuthorProfile(email: string): UserProfileDTO {
     return this.service.getProfileByEmail(email);
+  }
+
+  public getAuthorColor(stringToHash: string): string | undefined {
+
+    let hash = 0;
+    for (let i = 0; i < stringToHash.length; i++) {
+        hash = hash + stringToHash.charCodeAt(i);
+    }
+
+    let colorIndex = Math.abs(hash % this.lineColors.length);
+
+    return this.lineColors.at(colorIndex) + '6e';
   }
 
   private async fetchPostsNextPage(): Promise<CompletionStatusInformation> {
@@ -136,6 +160,26 @@ export class PostsComponent implements OnInit, OnDestroy {
       this.filterResultsByAuthor = routeAuthorParam;
     }
     this.onlyListOwnPosts = ownPostsUrlMatch;
+  }
+
+  public isUserAuthenticated(): boolean {
+    return this.authenticatedUser.hasToken();
+  }
+
+  public isLoadButtonDisabled(): boolean {
+    return this.loadButtonDisabled;
+  }
+
+  public getMessage(): string | undefined {
+    return this.message;
+  }
+
+  public fetchErrorHappened(): boolean {
+    return this.fetchError;
+  }
+
+  public isFetchMessageVisible(): boolean {
+    return this.fetchMessageVisible;
   }
 
 }
